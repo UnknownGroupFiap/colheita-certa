b788bffbb8c538069a2e90c6dc559b9eb3923e3e
fullzer4 <gabrielpelizzaro@gmail.com>
2025-11-08 18:59:58 -0300

    feat: organize repository structure and refactor CSS architecture

---
+++ flake.nix
@@ -0,0 +1,27 @@
+
+{
+  description = "Colheita Certa - Ambiente de desenvolvimento";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            nodePackages.pnpm
+            git
+          ];
+        };
+      }
+    );
+}
